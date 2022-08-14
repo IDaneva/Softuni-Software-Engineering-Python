b1a91fb624de0f9c098bab93597a0f0d33a4168e
IDaneva <daneva_n@abv.bg>
2022-08-14 22:03:45 +0300

    Create vowels_sum.py

---
+++ Programming Basics/4 for loops/vowels_sum.py
@@ -0,0 +1,16 @@
+text = input()
+total = 0
+
+for ch in text:
+    if ch == "a":
+        sum += 1
+    elif ch == "e":
+        sum += 2
+    elif ch == "i":
+        sum += 3
+    elif ch == "o":
+        sum += 4
+    elif ch == "u":
+        sum += 5
+
+print(total)
