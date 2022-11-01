b83653642ef86068ff5b0cd1c9acf0805a205638
IDaneva <daneva_n@abv.bg>
2022-11-01 09:14:15 +0200

    Create 06_stack_of_strings.py

---
+++ Python OOP module/Inheritance/06_stack_of_strings.py
@@ -0,0 +1,21 @@
+class Stack:
+
+    def __init__(self):
+        self.data = []
+
+    def push(self, element):
+        self.data.append(element)
+
+    def pop(self):
+        a = self.data.pop()
+        return a
+
+    def top(self):
+        return self.data[-1]
+
+    def is_empty(self):
+        return len(self.data) == 0
+
+    def __str__(self):
+        return f"[{', '.join(self.data[::-1])}]"
+
