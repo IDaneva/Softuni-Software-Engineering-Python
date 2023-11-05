ee5378f0744a3dfcbfd3c5a5a9b1c9b14ed8b5eb
IDaneva <daneva_n@abv.bg>
2023-11-05 15:55:56 +0200

    added db models

--- Python DB/Python ORM - October 2023/Exercise Data Operations in Django with Queries/skeleton_exercise/caller.py
+++ Python DB/Python ORM - October 2023/Exercise Data Operations in Django with Queries/skeleton_exercise/caller.py
@@ -1,11 +1,12 @@
 import os
 import django
+from django.db.models import F
 
 # Set up Django
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "orm_skeleton.settings")
 django.setup()
 
-from main_app.models import Pet, Artifact, Location
+from main_app.models import Pet, Artifact, Location, Car, Task
 
 
 def create_pet(name: str, species: str):
@@ -13,7 +14,7 @@ def create_pet(name: str, species: str):
     return f"{pet.name} is a very cute {pet.species}!"
 
 
-def create_artifact(name:str, origin:str, age:int, description:str, is_magical:bool):
+def create_artifact(name: str, origin: str, age: int, description: str, is_magical: bool):
     Artifact.objects.create(name=name, origin=origin, age=age, description=description, is_magical=is_magical)
     return f"The artifact {name} is {age} years old!"
 
@@ -23,4 +24,57 @@ def delete_all_artifacts():
 
 
 def show_all_locations():
-    locations = Location.objects.all()
+    locations = Location.objects.all().order_by("-id")
+    return '\n'.join(str(l) for l in locations)
+
+
+def new_capital():
+    city = Location.objects.first()
+    city.is_capital = True
+    city.save()
+
+
+def get_capitals():
+    return Location.objects.filter(is_capital=True).values("name")
+
+
+def delete_first_location():
+    city = Location.objects.first()
+    city.delete()
+
+
+def apply_discount():
+    cars = Car.objects.all()
+    for car in cars:
+        discount = sum(int(x) for x in str(car.year)) / 100
+        car.price_with_discount = float(car.price) - (float(car.price) * discount)
+        car.save()
+
+
+def get_recent_cars():
+    return Car.objects.filter(year__gte=2020).values('model', 'price_with_discount')
+
+
+def delete_last_car():
+    Car.objects.last().delete()
+
+
+def show_unfinished_tasks():
+    tasks = Task.objects.filter(is_finished=False)
+    return '\n'.join(str(x) for x in tasks)
+
+
+def complete_odd_tasks():
+    tasks = Task.objects.all()
+    for task in tasks:
+        if task.id % 2 != 0:
+            task.is_finished = True
+            task.save()
+
+
+def encode_and_replace(text: str, task_title: str):
+    decoded_text = ''.join(chr(ord(x) - 3) for x in text)
+    tasks_with_matching_title = Task.objects.filter(title=task_title)
+    for task in tasks_with_matching_title:
+        task.description = decoded_text
+        task.save()
--- Python DB/Python ORM - October 2023/Exercise Data Operations in Django with Queries/skeleton_exercise/main_app/models.py
+++ Python DB/Python ORM - October 2023/Exercise Data Operations in Django with Queries/skeleton_exercise/main_app/models.py
@@ -18,6 +18,40 @@ class Location(models.Model):
     name = models.CharField(max_length=100)
     region = models.CharField(max_length=50)
     population = models.PositiveIntegerField()
-    description = models.PositiveIntegerField()
+    description = models.TextField()
     is_capital = models.BooleanField(default=False)
 
+    def __str__(self):
+        return f"{self.name} has a population of {self.population}!"
+
+
+class Car(models.Model):
+    model = models.CharField(max_length=40)
+    year = models.PositiveIntegerField()
+    color = models.CharField(max_length=40)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    price_with_discount = models.DecimalField(max_digits=10, decimal_places=2, default=0)
+
+
+class Task(models.Model):
+    title = models.CharField(max_length=25)
+    description = models.TextField()
+    due_date = models.DateField()
+    is_finished = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f"Task - {self.title} needs to be done until {self.due_date}!"
+
+
+class HotelRoom(models.Model):
+    ROOM_TYPE_CHOICE = (
+        ("Standard", "Standard"),
+        ("Deluxe", "Deluxe"),
+        ("Suite", "Suite"),
+    )
+    room_number = models.PositiveIntegerField()
+    room_type = models.CharField(choices=ROOM_TYPE_CHOICE, max_length=20)
+    capacity = models.PositiveIntegerField()
+    amenities = models.TextField()
+    price_per_night = models.DecimalField(max_digits=8, decimal_places=2)
+    is_reserved = models.BooleanField(default=False)
