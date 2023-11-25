9d0cb1da80d5d17559ef9a7e33f31cbbfd6bfadd
IDaneva <daneva_n@abv.bg>
2023-11-25 12:36:30 +0200

    exam preparation

--- Python DB/Python ORM - October 2023/Exercise Django Models Relations/skeleton_exercise/main_app/admin.py
+++ Python DB/Python ORM - October 2023/Exercise Django Models Relations/skeleton_exercise/main_app/admin.py
@@ -1,3 +1,24 @@
 from django.contrib import admin
 
-# Register your models here.
+from main_app.models import Car
+
+
+@admin.register(Car)
+class CarAdmin(admin.ModelAdmin):
+    list_display = ['model', 'year', 'owner', 'car_details']
+
+    @staticmethod
+    def car_details(obj):
+        try:
+            owner_name = obj.owner.name
+        except:
+            owner_name = "No owner"
+
+        try:
+            registration = obj.registration.registration_number
+        except:
+            registration = "No registration number"
+
+        return f"Owner: {owner_name}, Registration: {registration}"
+
+    car_details.short_description = "Car Details"
---
+++ Python DB/Python ORM - October 2023/Exercise Models Inheritance and Customization/skeleton_exercise/main_app/models.py
@@ -0,0 +1,112 @@
+from django.core.exceptions import ValidationError
+from django.db import models
+
+
+class BaseCharacter(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+
+    class Meta:
+        abstract = True
+
+
+class Mage(BaseCharacter):
+    elemental_power = models.CharField(max_length=100)
+    spellbook_type = models.CharField(max_length=100)
+
+
+class Assassin(BaseCharacter):
+    weapon_type = models.CharField(max_length=100)
+    assassination_technique = models.CharField(max_length=100)
+
+
+class DemonHunter(BaseCharacter):
+    weapon_type = models.CharField(max_length=100)
+    demon_slaying_ability = models.CharField(max_length=100)
+
+
+class TimeMage(Mage):
+    time_magic_mastery = models.CharField(max_length=100)
+    temporal_shift_ability = models.CharField(max_length=100)
+
+
+class Necromancer(Mage):
+    raise_dead_ability = models.CharField(max_length=100)
+
+
+class ViperAssassin(Assassin):
+    venomous_strikes_mastery = models.CharField(max_length=100)
+    venomous_bite_ability = models.CharField(max_length=100)
+
+
+class ShadowbladeAssassin(Assassin):
+    shadowstep_ability = models.CharField(max_length=100)
+
+
+class VengeanceDemonHunter(DemonHunter):
+    vengeance_mastery = models.CharField(max_length=100)
+    retribution_ability = models.CharField(max_length=100)
+
+
+class FelbladeDemonHunter(DemonHunter):
+    felblade_ability = models.CharField(max_length=100)
+
+
+class UserProfile(models.Model):
+    username = models.CharField(max_length=70, unique=True)
+    email = models.EmailField(unique=True)
+    bio = models.TextField(null=True, blank=True)
+
+
+class Message(models.Model):
+    sender = models.ForeignKey(to=UserProfile, on_delete=models.CASCADE, related_name='sent_messages')
+    receiver = models.ForeignKey(to=UserProfile, on_delete=models.CASCADE, related_name='received_messages')
+    content = models.TextField()
+    timestamp = models.DateTimeField(auto_now_add=True)
+    is_read = models.BooleanField(default=False)
+
+    def mark_as_read(self):
+        self.is_read = True
+
+    def mark_as_unread(self):
+        self.is_read = False
+
+    def reply_to_message(self, reply_content, receiver):
+        new_message = Message(sender=self.receiver, receiver=receiver, content=reply_content)
+        return new_message
+
+    def forward_message(self, sender, receiver):
+        new_message = Message(sender=sender, receiver=receiver, content=self.content)
+        return new_message
+
+
+class StudentIDField(models.PositiveIntegerField):
+    def to_python(self, value):
+        try:
+            return int(value)
+        except ValueError:
+            pass  # better raise ValidationError
+
+
+class Student(models.Model):
+    name = models.CharField(max_length=100)
+    student_id = StudentIDField()
+
+
+class MaskedCreditCardField(models.CharField):
+    def to_python(self, value):
+        if not isinstance(value, str):
+            raise ValidationError("The card number must be a string")
+
+        if not value.isdigit():
+            raise ValidationError("The card number must contain only digits")
+
+        if len(value) != 16:
+            raise ValidationError("The card number must be exactly 16 characters long")
+
+        return f"****-****-****-{value[-4:]}"
+
+
+class CreditCard(models.Model):
+    card_owner = models.CharField(max_length=100)
+    card_number = MaskedCreditCardField(max_length=20)
